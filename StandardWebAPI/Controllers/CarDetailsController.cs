4c6ad3d66953bde90c8df9d88dbb6fd3bda53e51
Dakota Bloodworth <dblood2244@gmail.com>
2022-07-05 11:29:51 -0500

    Add project files.

---
+++ StandardWebAPI/Controllers/CarDetailsController.cs
@@ -0,0 +1,78 @@
+﻿using StandardWebAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using StandardWebAPI.Models;
+
+namespace StandardWebAPI.Controllers
+{
+    public class CarDetailsController : ApiController
+    {
+        public IEnumerable<CarStock>GetAllCarDetails()
+        {
+            CarStock ST = new CarStock();
+            CarStock ST1 = new CarStock();
+            List<CarStock> carlist = new List<CarStock>();
+
+            ST.CarName = "Ford";
+            ST.CarPrice ="Thirty thousnd";
+            ST.CarModel ="Bronco";
+            ST.CarColor ="Shit Brown";
+
+            ST1.CarName = "Dodge";
+            ST1.CarPrice = "Fifty thousnd";
+            ST1.CarModel = "Ram";
+            ST1.CarColor = "Rotyal Bluse";
+
+            carlist.Add(ST);
+            carlist.Add(ST1);
+            return carlist;
+        }
+        // GET api/<controller>
+        public IEnumerable<CarStock> Get(int id)
+        {
+            CarStock ST = new CarStock();
+            CarStock ST1 = new CarStock();
+            List<CarStock> carlist = new List<CarStock>();
+            if (id == 1)
+            {
+                ST.CarName = "Ford";
+                ST.CarPrice = "Thirty thousnd";
+                ST.CarModel = "Bronco";
+                ST.CarColor = "Shit Brown";
+                carlist.Add(ST);
+            }
+            else
+            {
+                ST1.CarName = "Dodge";
+                ST1.CarPrice = "Fifty thousnd";
+                ST1.CarModel = "Ram";
+                ST1.CarColor = "Rotyal Bluse";
+
+
+                carlist.Add(ST1);
+            }
+                return carlist;
+            
+        }
+        // POST api/<controller>
+        public void PostCar([FromBody]string value)
+        {
+        }
+
+        // PUT api/<controller>/5
+        [HttpPut]
+        public void Putcar(int id, [FromBody] CarStock cs)
+        {
+        }
+
+        // DELETE api/<controller>/5
+        [HttpDelete]
+        public void Deletecar(int id)
+        {
+        }
+    }
+}
---
+++ StandardWebAPI/Models/CarStock.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace StandardWebAPI.Models
+{
+    public class CarStock
+    {public string CarName { get; set; }
+     public string CarModel { get; set; }
+     public string CarPrice { get; set; }
+     public string CarColor { get; set; }
+    }
+}
